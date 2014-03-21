93042afa26c02adb9e0e0a50a1c133ebc793b2b9
Scott Slack <kd7vbs@gmail.com>
2014-03-20 20:58:17 -0700

    Extened test case to include merge sort

--- TestHarness/TestCases/TestCaseSort.cs
+++ TestHarness/TestCases/TestCaseSort.cs
@@ -121,4 +121,49 @@ namespace Tests {
             this.coverage.Add(TestCoverage.F_T_sort_order_by);
         }
     }
+
+    [Export(typeof(ISyncTestCase))]
+    public class test_sort_merge : ISyncTestCase {
+        private const string _Name = "test_sort_merge,F_T_sort_merge";
+        private const string _Description = "sort a sequence of random integers using merge sort";
+        public string TestFile { get { return "TestCaseSort.cs"; } }
+        public Func<bool> Run { get; private set; }
+        private static bool _Run() {
+            bool result = true;
+            int[] unsorted = new int[16] { 9, 4, 6, 2, 1, 3, 7, 8, 4, 3, 7, 1,-6, 7,11, 4 };
+            int[] sorted = new int[16] { 11,9,8,7, 7,7,6,4, 4,4,3,3, 2,1,1,-6 };
+            int[] sort_result = new int[16];
+            F<int>.merge_sort(F.compare_int, sort_result, unsorted);
+            for (int i = 0; i < 16; i++) {
+                result = result && sort_result[i] == sorted[i];
+            }
+            return result;
+        }
+        private IList<int> coverage = new List<int>();
+        private IList<int> feature = new List<int>();
+        public int ID { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        public IEnumerable<int> Coverage { get { return this.coverage; } }
+        public IEnumerable<int> Feature { get { return this.feature; } }
+        public test_sort_merge() {
+            this.ID = 0;
+            this.Name = _Name;
+            this.Description = _Description;
+            this.Run = _Run;
+
+            this.feature.Add(TestCoverage.Test_All);
+            this.feature.Add(TestCoverage.F_T);
+            this.feature.Add(TestCoverage.F_T_sort);
+            this.feature.Add(TestCoverage.F_T_sort_merge);
+
+            this.coverage.Add(TestCoverage.F);
+            this.coverage.Add(TestCoverage.F_compare);
+            this.coverage.Add(TestCoverage.F_compare_int);
+            this.coverage.Add(TestCoverage.F_T);
+            this.coverage.Add(TestCoverage.F_T_sort);
+            this.coverage.Add(TestCoverage.F_T_sort_merge);
+        }
+    }
 }
